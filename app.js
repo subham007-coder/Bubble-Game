51d3dbb85c292247eb13b79e9cfe989c3bc1cea7
subham007-coder <dsubham109@gmail.com>
2023-10-26 21:29:34 +0530

    work

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let store = "";
+
+let time = 61;
+
 function randomBubble() {
-    let store = "";
 
     for (let i = 1; i <= 144; i++) {
         let randomNum = Math.floor(Math.random() * 10) + 1;
@@ -9,4 +12,21 @@ function randomBubble() {
     let Full = document.querySelector("#full").innerHTML = store;
 }
 
+
+
+function runTime() {
+  let Timer = setInterval(() => {
+        if (time > 0) {
+            time--;
+            let timeBox = document.querySelector("#Timer").innerHTML = time;
+        } else{
+            clearInterval(Timer);
+        }
+    }, 1000);
+}
+
+
+
 randomBubble();
+
+runTime();
