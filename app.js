400cb25046218b3f37f5a1bf9857d060151b0e18
subham007-coder <dsubham109@gmail.com>
2023-10-26 22:42:20 +0530

    done

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ let randomHit = 0;
 
 function randomBubble() { // for all bubble;
 
-    for (let i = 1; i <= 144; i++) {
+    for (let i = 1; i <= 270; i++) {
         let randomNum = Math.floor(Math.random() * 10) + 1;
         store += `<div class="bubble">${randomNum}</div>`;
     }
 
     document.querySelector("#full").innerHTML = store;
 }
-
-
 
 function runTime() { // time run function;
     let Timer = setInterval(() => {
@@ -25,6 +23,7 @@ function runTime() { // time run function;
             let timeBox = document.querySelector("#Timer").innerHTML = time;
         } else {
             clearInterval(Timer);
+            document.querySelector("#full").innerHTML = `<h1>Game Over! Try Again`;
         }
     }, 1000);
 }
@@ -40,7 +39,7 @@ function scoreBox() { // score update;
 }
 
 document.querySelector("#full").addEventListener("click", (bubble) => {
-    let bubbleText = Number(bubble.target.textContent);
+    let bubbleText = parseInt(bubble.target.textContent);
     console.log(typeof bubbleText);
     console.log(bubbleText);
 
